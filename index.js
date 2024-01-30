64252997c35fe9b190210a400a9209dfc9739c75
blue031415 <blue031415@gmail.com>
2024-01-30 18:04:23 +0900

    file comvert

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const readfile = document.getElementById("abc");
 
 addEventListener("load",function(){
-    console.log("Hello world")
+    console.log("Hello world");
 });
 
 async function DisplayConsole(){
@@ -12,3 +12,44 @@ async function DisplayConsole(){
 }
 
 readfile.addEventListener("click", DisplayConsole());
+
+document.addEventListener('DOMContentLoaded', function() {
+    var fileInput = document.getElementById('fileInput');
+    var convertButton = document.getElementById('convertButton');
+    var outputDiv = document.getElementById('output');
+
+    fileInput.addEventListener('change', handleFileSelect);
+    convertButton.addEventListener('click', convertToJSON);
+
+    function handleFileSelect(event) {
+        var file = event.target.files[0];
+        var reader = new FileReader();
+
+        reader.onload = function(event) {
+            var csvData = event.target.result;
+            outputDiv.innerText = csvData;
+        };
+
+        reader.readAsText(file);
+    }
+
+    function convertToJSON() {
+        var csvData = outputDiv.innerText;
+        var lines = csvData.split('\n');
+        var result = [];
+
+        var headers = lines[0].split(',');
+        for (var i = 1; i < lines.length; i++) {
+            var obj = {};
+            var currentLine = lines[i].split(',');
+
+            for (var j = 0; j < headers.length; j++) {
+                obj[headers[j]] = currentLine[j];
+            }
+
+            result.push(obj);
+        }
+
+        outputDiv.innerText = JSON.stringify(result, null, 2);
+    }
+});
