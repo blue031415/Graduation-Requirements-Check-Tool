d001929049d750e1cc11f7ddfe4526a047599b5d
blue031415 <blue031415@gmail.com>
2024-01-30 16:21:11 +0900

    test JSON

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const readfile = document.getElementById("abc");
+
 addEventListener("load",function(){
-    console.log("Hello world!")
-})
+    console.log("Hello world")
+});
+
+async function DisplayConsole(){
+    console.log("foo");
+    const res = await fetch("https://blue031415.github.io/Graduation-Requirements-Check-Tool/grad.json");
+    const users = await res.json();
+    console.log(users);
+}
+
+readfile.addEventListener("click", DisplayConsole());
